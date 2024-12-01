bd9d6cf3166f68cb246b2d21a9dbb93a0b978583
msteigerwald <cio@agloa.org>
2024-12-01 17:46:28 -0600

    Move most of Person to Form

--- CRM/Tournament/Form/Form.php
+++ CRM/Tournament/Form/Form.php
@@ -8,21 +8,98 @@
  * @version 1.0
  * @author msteigerwald
  */
+
+use CRM_Tournament_ExtensionUtil as E;
   
 class Tournament_Core_Form extends CRM_Core_Form
 {
   protected $_values;
   protected $_id;
   protected $_fieldNames;
+  protected $_recordName;
+  protected $_updateAction;
 
   public function preProcess()
   {
     parent::preProcess();
 
     $this->_action = CRM_Utils_Request::retrieve('action', 'String', $this, FALSE, 'update');
+
+    if ($this->_action == CRM_Core_Action::ADD) {
+      $this->addRecord();
+    } else if ($this->_action == CRM_Core_Action::UPDATE) {
+      $this->updateRecord();
+    }
+ 
   }
 
-  protected function update(){
+  public function buildQuickForm()
+  {
+    $this->applyFilter('__ALL__', 'trim');
+    $this->addButtons(array(
+      array(
+        'type' => 'submit',
+        'name' => E::ts('Save'),
+        'isDefault' => TRUE,
+      ),
+    ));
+
+    // export form elements
+    $this->assign('elementNames', $this->getRenderableElementNames());
+  }
+
+  public function postProcess()
+  {
+    foreach ($this->_fieldNames as $fieldName) {
+      $this->_updateAction->addValue($fieldName, $this->_values[$fieldName]);
+    }
+
+    $this->_updateAction->execute();
+
+    $session = CRM_Core_Session::singleton();
+    $session->setStatus($this->_recordName, "$this->_name Saved", 'success');
+
+    $this->updateTitle();
+  }
+
+  /**
+   * Set default values for the form.
+   *
+   * Note that in edit/view mode the default values are retrieved from the database
+   */
+  public function setDefaultValues()
+  {
+    $defaults = $this->_values;
+    return $defaults;
+  }  
+
+  /**
+   * Default form context used as part of addField()
+   */
+  public function getDefaultContext(): string
+  {
+    return 'create';
+  }
+
+  public function getRenderableElementNames()
+  {
+    // The _elements list includes some items which should not be
+    // auto-rendered in the loop -- such as "qfKey" and "buttons".  These
+    // items don't have labels.  We'll identify renderable by filtering on
+    // the 'label'.
+    $elementNames = array();
+    foreach ($this->_elements as $element) {
+      /** @var HTML_QuickForm_Element $element */
+      $label = $element->getLabel();
+      if (!empty($label)) {
+        $elementNames[] = $element->getName();
+      }
+    }
+    return $elementNames;
+  }
+
+  protected function updateRecord()
+  {
     $getAction = $this->getGetSingleRecordAction();
 
     foreach ($this->_fieldNames as &$fieldName) {
@@ -42,12 +119,30 @@ class Tournament_Core_Form extends CRM_Core_Form
       CRM_Core_Error::statusBounce(ts("Could not find $this->_name with id = $this->_id"));
     }
 
-
-    if (!CRM_Core_Permission::check("edit " . $this->getDefaultEntity() . "s", $this->_id)) {
+    if (!CRM_Core_Permission::check('edit ' . $this->getDefaultEntity() . 's', $this->_id)) {
       CRM_Utils_System::permissionDenied();
       CRM_Utils_System::civiExit();
     }
 
     $this->updateTitle();
+  }
+
+  protected function updateTitle()
+  {
+    $this->setTitle(ts('Edit %1', [1 => $this->_recordName]));
+  }
+
+  protected function addRecord()
+  {
+    // check for add contacts permissions
+    if (!CRM_Core_Permission::check('add ' . $this->getDefaultEntity() . 's')) {
+      CRM_Utils_System::permissionDenied();
+      CRM_Utils_System::civiExit();
+    }
+
+    $this->setTitle(ts('New %1', [1 => $this->_name]));
+
+    $session = CRM_Core_Session::singleton();
+    $session->pushUserContext(CRM_Utils_System::url('civicrm/dashboard', 'reset=1'));
   }
 }
--- CRM/Tournament/Form/Person.php
+++ CRM/Tournament/Form/Person.php
@@ -1,38 +1,11 @@
 <?php
 require_once "Form.php";
-use CRM_Tournament_ExtensionUtil as E;
 
-/**
- * Form controller class
- *
- * @see https://docs.civicrm.org/dev/en/latest/framework/quickform/
- */
 class CRM_Tournament_Form_Person extends Tournament_Core_Form {
   public function __construct($state, $action, $method, $name)
   {
     parent::__construct($state, $action, $method, $name);
-
     $this->_fieldNames = array('first_name', 'middle_name', 'last_name', 'birth_date', 'gender_id', 'prefix_id', 'suffix_id');
-  }
-
-  public function preProcess()
-  {
-    parent::preProcess();
-
-    if ($this->_action == CRM_Core_Action::ADD) {
-      // check for add contacts permissions
-      if (!CRM_Core_Permission::check('add contacts')) {
-        CRM_Utils_System::permissionDenied();
-        CRM_Utils_System::civiExit();
-      }
-
-      $this->setTitle(ts('New %1', [1 => $this->_name]));
-
-      $session = CRM_Core_Session::singleton();
-      $session->pushUserContext(CRM_Utils_System::url('civicrm/dashboard', 'reset=1'));
-    } else if ($this->_action == CRM_Core_Action::UPDATE) {
-      $this->update();
-    }
   }
 
   protected function getGetSingleRecordAction(){
@@ -43,7 +16,6 @@ class CRM_Tournament_Form_Person extends Tournament_Core_Form {
   }
 
   public function buildQuickForm() {
-    $this->applyFilter('__ALL__', 'trim');
     $entity = $this->getDefaultEntity();
 
     $this->addField('last_name', ['placeholder' => ts('Last Name'), 'label' => ts('Last Name')], true);
@@ -57,71 +29,14 @@ class CRM_Tournament_Form_Person extends Tournament_Core_Form {
     $this->addField('prefix_id', ['entity' => $entity, 'placeholder' => ts('Prefix'), 'label' => ts('Prefix')]);
     $this->addField('suffix_id', ['entity' => $entity, 'placeholder' => ts('Suffix'), 'label' => ts('Suffix')]);
 
-    $this->addButtons(array(
-      array(
-        'type' => 'submit',
-        'name' => E::ts('Save'),
-        'isDefault' => TRUE,
-      ),
-    ));
-
-    // export form elements
-    $this->assign('elementNames', $this->getRenderableElementNames());
-
-    //$this->assign('contactId', current);
     parent::buildQuickForm();
   }
 
   public function postProcess() {
     $this->_values = $this->exportValues();
-
-    \Civi\Api4\Individual::update(FALSE)
-      ->addValue('first_name', $this->_values['first_name'])
-      ->addValue('middle_name', $this->_values['middle_name'])
-      ->addValue('last_name', $this->_values['last_name'])
-      ->addValue('birth_date', $this->_values['birth_date'])
-      ->addValue('gender_id', $this->_values['gender_id'])
-      ->addValue('prefix_id', $this->_values['prefix_id'])
-      ->addValue('suffix_id', $this->_values['suffix_id'])
-      ->addWhere('id', '=', $this->getContactID())
-      ->execute();
-
-    $session = CRM_Core_Session::singleton();
-    $session->setStatus($this->displayName(), "$this->_name Saved", 'success');
-
-    $this->updateTitle();
-
+    $this->_recordName = $this->displayName();
+    $this->_updateAction = \Civi\Api4\Individual::update(FALSE)->addWhere('id', '=', $this->_id);
     parent::postProcess();
-  }
-
-  /**
-   * Get the fields/elements defined in this form.
-   *
-   * @return array (string)
-   */
-  
-  public function getRenderableElementNames() {
-    // The _elements list includes some items which should not be
-    // auto-rendered in the loop -- such as "qfKey" and "buttons".  These
-    // items don't have labels.  We'll identify renderable by filtering on
-    // the 'label'.
-    $elementNames = array();
-    foreach ($this->_elements as $element) {
-      /** @var HTML_QuickForm_Element $element */
-      $label = $element->getLabel();
-      if (!empty($label)) {
-        $elementNames[] = $element->getName();
-      }
-    }
-    return $elementNames;
-  }
-
-  /**
-   * Default form context used as part of addField()
-   */
-  public function getDefaultContext(): string
-  {
-    return 'create';
   }
 
   /**
@@ -130,23 +45,6 @@ class CRM_Tournament_Form_Person extends Tournament_Core_Form {
   public function getDefaultEntity()
   {
     return 'contact';
-  }
-
-  /**
-   * Set default values for the form.
-   *
-   * Note that in edit/view mode the default values are retrieved from the database
-   */
-  public function setDefaultValues()
-  {
-    $defaults = $this->_values;
-    return $defaults;
-  }
-
-  protected function updateTitle(){     
-    $displayName = 
-    $title = ts('Edit %1', [1 => $this->displayName()]);
-    $this->setTitle($title);
   }
 
   private function displayName(){
