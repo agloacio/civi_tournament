9b09cb23783fe2456cbecc8f875c6c9dfbaa15f6
agloacio <131702060+agloacio@users.noreply.github.com>
2024-12-26 18:59:12 -0600

    Add Tournaments navigation menu.

--- civi_tournament.php
+++ civi_tournament.php
@@ -30,3 +30,38 @@ function civi_tournament_civicrm_install(): void {
 function civi_tournament_civicrm_enable(): void {
   _civi_tournament_civix_civicrm_enable();
 }
+
+/**
+ * Implements hook_civicrm_navigationMenu().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_navigationMenu
+ */
+function civi_tournament_civicrm_navigationMenu(&$menu)
+{
+  _civi_tournament_civix_insert_navigation_menu($menu, '', array(
+    'label' => E::ts('Tournaments'),
+    'name' => 'tournament',
+    'permission' => 'access CiviEvent',
+    'operator' => 'OR',
+    'separator' => 0,
+    'icon' => 'crm-i fa-calendar',
+  ));
+
+  _civi_tournament_civix_insert_navigation_menu($menu, 'tournament', array(
+    'label' => E::ts('Dashboard'),
+    'name' => 'extensions',
+    'url' => 'civicrm/tournament',
+    'permission' => 'access CiviEvent',
+    'operator' => 'OR',
+    'separator' => 0
+  ));
+
+  _civi_tournament_civix_insert_navigation_menu($menu, 'tournament', array(
+    'label' => E::ts('Person'),
+    'name' => 'person',
+    'url' => 'civicrm/tournament/person',
+    'permission' => 'access CiviCRM',
+    'operator' => 'OR',
+    'separator' => 0,
+  ));
+}
