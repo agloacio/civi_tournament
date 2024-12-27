e0995386f13923d4bba2d861a9c0bc7bb7456773
agloacio <131702060+agloacio@users.noreply.github.com>
2024-12-26 18:55:29 -0600

    Add classes for Session, User and Form/Field

---
+++ CRM/CiviTournament/User.php
@@ -0,0 +1,19 @@
+<?php
+use \Civi\Api4\Individual as Api;
+require_once "CRM/CiviTournament/Session.php";
+
+class User
+{
+  public $_id;
+  public $_name;
+  public $_contactUrl;
+
+  public function __construct($id = null)
+  {
+    if (empty($id)) {
+      $id = Session::getLoggedInContactID();
+    }
+
+    $this->_id = $id;
+  }
+}
