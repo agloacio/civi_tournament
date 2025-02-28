d83e69b263c5d52795d67d19425b71fef7a50511
msteigerwald <cio@agloa.org>
2025-02-28 17:46:30 -0600

    Remove copied Billing Organization fields

--- CRM/CiviTournament/Form/RegistrationGroup.php
+++ CRM/CiviTournament/Form/RegistrationGroup.php
@@ -5,7 +5,7 @@ require_once "Field.php";
 /**
  * Registration Group Form controller class
  *
- * Create/Update Billing Organizations.
+ * Create/Update Registration Groups.
  * 
  * @see https://docs.civicrm.org/dev/en/latest/framework/quickform/
  *
@@ -23,17 +23,7 @@ class CRM_CiviTournament_Form_RegistrationGroup extends CRM_CiviTournament_Form_
     $this->_fields = array_merge(
       $this->_fields,
       array(
-        new Field($entity, 'organization_name', 'Name', 'Text', TRUE),
-        new Field($entity, 'email_primary.email', 'Email', 'Text', TRUE),
-        new Field($entity, 'phone_billing.phone', 'Main Phone', 'Text', TRUE),
-        new Field($entity, 'phone_primary.phone', 'On Site Phone', 'Text', TRUE, 'How can we reach you at the tournament?'),
-        new Field($entity, 'address_primary.street_address', 'Address', 'Text', TRUE),
-        new Field($entity, 'address_primary.supplemental_address_1', 'Address (cont.)', 'Text', FALSE),
-        new Field($entity, 'address_primary.city', 'City', 'Text', TRUE),
-        new Field($entity, 'address_primary.country_id', 'Country', CRM_CiviTournament_Form::COUNTRY_SELECT, FALSE),
-        new Field($entity, 'address_primary.state_province_id', 'State', CRM_CiviTournament_Form::STATE_PROVINCE_SELECT, TRUE),
-        new Field($entity, 'address_primary.postal_code', 'Postal Code', 'Text', TRUE),
-        new Field($entity, 'address_primary.postal_code_suffix', 'Postal Code Suffix', 'Text', FALSE),
+        new Field($entity, 'organization_name', 'Name', 'Text', TRUE)
       )
     );
   }
