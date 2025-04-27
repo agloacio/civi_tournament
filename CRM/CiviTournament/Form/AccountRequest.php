5cb23f359c5506595bb37d16fbc82870e09ec1e6
Michael Steigerwald <msteigerwald@bayandbay.com>
2025-04-27 14:28:19 -0400

    Select options for Salutations and Generations

--- CRM/CiviTournament/Form/AccountRequest.php
+++ CRM/CiviTournament/Form/AccountRequest.php
@@ -1,5 +1,7 @@
 <?php
 require_once "CRM/CiviTournament/Models/Settings/Genders.php";
+require_once "CRM/CiviTournament/Models/Settings/Salutations.php";
+require_once "CRM/CiviTournament/Models/Settings/Generations.php";
 
 /**
  * CRM_CiviTournament_Form_AccountRequest lets a new user apply for an account.
@@ -22,17 +24,38 @@ class CRM_CiviTournament_Form_AccountRequest extends CRM_Core_Form {
   }
 
   public function buildQuickForm() {
-    $required = true;  
+    $required = true;
+    $optional = false;
 
     $this->add('text', 'email', ts('Email'), array('placeholder' => ts('Your Email')), $required);
     $this->add('text', 'organizationName', ts('Organization Name (e.g., School or School District)'), array('placeholder' => ts('School or District Name')), $required);
+    $this->add(
+      'select',
+      'prefix_id',
+      ts('Salutation'),
+      array_merge([0 => '-- Select Salutation --'], Salutations::get())
+    );
     $this->add('text', 'firstName', ts('First Name'), array('placeholder' => ts('First Name')), $required);
     $this->add('text', 'lastName', ts('Last Name'), array('placeholder' => ts('Last Name')), $required);
-    $this->add('select', 'gender_id', ts('Gender'), array_merge([0 => '-- Select Gender --'], Genders::get()));
+    $this->add(
+      'select',
+      'suffix_id',
+      ts('Generation'),
+      array_merge([0 => '-- Select Generation --'], Generations::get())
+    );
+    $this->add(
+      'select',
+      'gender_id',
+      ts('Gender'),
+      array_merge([0 => '-- Select Gender --'], Genders::get()),
+      $optional,
+      ['title' => ts('In case we need to assign roommates for housing.')]
+    );
     $this->add('text', 'address', ts('Address'), array('placeholder' => ts('Address')), $required);
     $this->add('text', 'city', ts('City'), array('placeholder' => ts('City')), $required);
     $this->add('text', 'postalCode', ts('Zip'), array('placeholder' => ts('Zip')), $required);
-    $this->add('text', 'phone', ts('Phone'), array('placeholder' => ts('Phone')), $required);
+    $this->add('text', 'phone', ts('Landline'), array('placeholder' => ts('Phone')), $required);
+    $this->add('text','mobile_phone', ts('Mobile Phone'), array('title' => ts('How can we reach you at the tournament?')));
 
     $this->applyFilter('__ALL__', 'trim');
     $this->addButtons([
